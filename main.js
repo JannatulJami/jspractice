cf8f98127263412d207b7c769aa41787427ec6d7
J. Jami <144722335+JannatulJami@users.noreply.github.com>
2025-10-21 14:01:08 -0400

    k

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ alert('Hello Jannatul');
 console.log('Chikini Chameli')
 console.error('This is an error')
 console.warning('this is a warning')
+
+//var let, const, to create variables
+let age = 50;
+
+console.log(age);
