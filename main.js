3d95ea7794485e7bafe68c9c66d4dd970db7b083
J. Jami <144722335+JannatulJami@users.noreply.github.com>
2025-10-21 17:51:43 -0400

    q

--- main.js
+++ main.js
@@ -84,3 +84,14 @@ function orderMyLogic(val) {
     }
 }
  console.log(orderMyLogic(3))
+
+ var names = ["Hole-in-one", "Eagle", "Birdie", "Par"];
+ function golfScore(par, strokes) {
+    if (strokes == 1) {
+        return names[0];
+    } else if (strokes <= par - 2) {
+        return names[1];
+    } else if (strokes <= par - 1) {
+        return names[2];
+    }
+}
